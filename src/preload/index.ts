7aa5bd58d1e2a5e0f7d268e7abafb0ab04c844b5
HasutoSasaki <hasuto.sa@gmail.com>
2025-05-16 07:32:55 +0900

    feat: Add readNote functionality to expose readNote method in contextBridge.

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,5 @@
 import { contextBridge, ipcRenderer } from 'electron'
-import { GetNotes } from '../shared/types'
+import { GetNotes, ReadNote } from '../shared/types'
 
 if (!process.contextId) {
   throw new Error('contextIsolation must be enabled in ')
@@ -9,6 +9,7 @@ try {
   contextBridge.exposeInMainWorld('context', {
     locale: navigator.language,
     getNotes: (...args: Parameters<GetNotes>) => ipcRenderer.invoke('getNotes', ...args),
+    readNote: (...args: Parameters<ReadNote>) => ipcRenderer.invoke('readNote', ...args),
   })
 } catch (error) {
   console.error('Failed to expose contextBridge:', error)
